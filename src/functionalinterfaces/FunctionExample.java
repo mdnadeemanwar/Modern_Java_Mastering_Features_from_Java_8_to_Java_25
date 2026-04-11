e84b597504cc96ccf5e267203fbfe9b74924fca3
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-11 23:43:46 +0530

    start working on the function interface

---
+++ src/functionalinterfaces/FunctionExample.java
@@ -0,0 +1,15 @@
+package functionalinterfaces;
+
+import java.util.function.Function;
+
+public class FunctionExample {
+
+    static Function<String, String> function = (name) -> name.toUpperCase();
+
+    static Function<String ,String> addsomestring = (name) -> name.toUpperCase().concat("java 17");
+
+    public static void main(String[] args) {
+        System.out.println("Result is ->" + function.apply("Java8"));
+        System.out.println("Concat string is ->"+addsomestring.apply("Java11"));
+    }
+}
