558463d9de6a947f2ad7a20df3b5ba088dd09106
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-23 13:27:30 +0530

    binary opertor maxby and minby complete

---
+++ src/functionalinterfaces/BinaryOperatorExample.java
@@ -0,0 +1,24 @@
+package functionalinterfaces;
+
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+
+//    static void binaryOperator() {}
+    static Comparator<Integer> comparator = (a,b)->a.compareTo(b);
+
+    public static void main(String[] args) {
+        BinaryOperator<Integer> binaryOperator = (a, b) -> a * b;
+        System.out.println(binaryOperator.apply(10, 20));
+
+        BinaryOperator<Integer> binaryOperator1 = (a, b) -> a * b;
+
+        BinaryOperator<Integer> maxBy = BinaryOperator.maxBy(comparator);
+        System.out.println(maxBy.apply(10, 20));
+
+        BinaryOperator<Integer> minBy = BinaryOperator.minBy(comparator);
+        System.out.println(minBy.apply(10, 20));
+
+    }
+}
