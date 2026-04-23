2bb833e5da3d0af99203e5c187e6d1284a9f2149
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-23 11:58:29 +0530

    complete one more example

---
+++ src/functionalinterfaces/FunctionStudentExample.java
@@ -0,0 +1,26 @@
+package functionalinterfaces;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionStudentExample {
+
+
+    static Function<List<Student>, Map<String, Double>> studentFunction = (student -> {
+        Map<String, Double> studentGradeMark = new HashMap<>();
+        student.forEach((studentName -> {
+            studentGradeMark.put(studentName.getName(), studentName.getGpa());
+        }));
+        return studentGradeMark;
+    });
+
+    public static void main(String[] args) {
+        System.out.println(studentFunction.apply(StudentDataBase.getAllStudents()));
+    }
+
+}
