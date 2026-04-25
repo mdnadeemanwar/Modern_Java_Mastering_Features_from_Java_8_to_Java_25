a68c2f2889becd17468e0c2e9e8076e6f893c292
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-25 13:48:11 +0530

    anyMatch allMatch , noneMatch method of  reduce  example is completed

---
+++ src/streams/StreamMatchExample.java
@@ -0,0 +1,38 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+public class StreamMatchExample {
+
+
+    //if all student gpa greater than 3.9 then return true
+
+    public static Boolean allStudentGPA(){
+       return StudentDataBase.getAllStudents().stream().allMatch(student ->student.getGpa()>=3.9);
+    }
+    public static Boolean anyStudentGPA(){
+       return StudentDataBase.getAllStudents().stream().anyMatch(student ->student.getGpa()>=3.9);
+    }
+    public static Boolean noneStudentGPA(){
+       return StudentDataBase.getAllStudents().stream().noneMatch(student ->student.getGpa()>=8.9);
+    }
+    public static void main(String[] args) {
+
+        //it will give false bcz not all student have gpa 3.9
+        System.out.println("Result is "+allStudentGPA());
+
+
+        //it will give true bcz one of the student has gpa 3.9
+        System.out.println("Result is "+anyStudentGPA());
+
+
+        //it will give false bcz not all student have gpa 8.9
+        System.out.println("Result is "+noneStudentGPA());
+
+
+
+
+
+    }
+}
