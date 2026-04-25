7521593bcba46c64587cfe49df8b973ae4f6cf77
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-25 14:11:03 +0530

    findFirst and findAny method of reduce example is completed

---
+++ src/streams/StreamFindFirstAndFindAnyExample.java
@@ -0,0 +1,33 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.Optional;
+
+public class StreamFindFirstAndFindAnyExample {
+
+    public  static Optional<Student> findFirstStudent(){
+       return StudentDataBase.getAllStudents().stream().filter(student -> student.getGpa()>3.9).findFirst();
+    }
+
+    public  static Optional<Student> findAnyStudent(){
+       return StudentDataBase.getAllStudents().stream().filter(student -> student.getGpa()>2.9).findAny();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Optional<Student> findFirstMatchStudent = findFirstStudent();
+        if(findFirstMatchStudent.isPresent()){
+            System.out.println(" Student is found -> "+findFirstMatchStudent.get());
+        }
+
+        Optional<Student> findAnyMatchStudent = findAnyStudent();
+        if(findAnyMatchStudent.isPresent()){
+            System.out.println(" Student with findAny ->"+findAnyMatchStudent.get());
+        }
+
+    }
+}
