c81a9b9e03ed5a6c8185ef962567517991edb624
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-30 18:43:36 +0530

    minBy and maxBy  method example is complete

---
+++ src/streamTerminalOperation/StreamMinMaxByExample.java
@@ -0,0 +1,32 @@
+package streamTerminalOperation;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class StreamMinMaxByExample {
+
+
+    public static Optional<Student> getMinimumGPAStudent(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.minBy(Comparator.comparing(Student::getGpa)));
+    }
+
+    public static Optional<Student> getMaximumGPAStudent(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.maxBy(Comparator.comparing(Student::getGpa)));
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(getMinimumGPAStudent());
+
+        System.out.println(getMaximumGPAStudent());
+    }
+}
