6d6e03f8673698882d4ec7bfb4b9fbe2cf7ee1d9
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-04-30 17:36:28 +0530

    boxing and unboxing complete

---
+++ src/numericStream/NumericStreamBoxingAndUnBoxingExample.java
@@ -0,0 +1,22 @@
+package numericStream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamBoxingAndUnBoxingExample {
+
+
+    public static List<Integer> boxing(){
+       return IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList());
+    }
+
+    public static int  unboxing(List<Integer> list){
+        return list.stream().mapToInt(Integer::intValue).sum();
+    }
+    public static void main(String[] args) {
+        System.out.println("BOxing example is "+boxing());
+
+        System.out.println("Unboxing example is "+unboxing(boxing()));
+    }
+}
