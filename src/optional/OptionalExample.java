f493cd4948a1cfeb5950e5216d5c567585d14bb1
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-05-05 18:59:51 +0530

    second commit

--- src/optional/OptionalExample.java
+++ src/optional/OptionalExample.java
@@ -3,26 +3,51 @@ package optional;
 import data.Student;
 import data.StudentDataBase;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class OptionalExample {
     //create a method for getStudentName
 
-    public static List<String> getOnlyStudentName(){
+    public static List<String> getOnlyStudentName() {
         List<String> names = StudentDataBase.getAllStudents().stream().map(Student::getName).collect(Collectors.toList());
         return names;
     }
-    public static String getStudentName(){
+
+    public static String getStudentName() {
         Student student = StudentDataBase.getAllStudents().get(0);
 
+        //put the student null thensee
+//        Student student = null;
 
-        if(student != null){
+        if (student != null) {
             return student.getName();
         }
         return null;
     }
+
+    public static Optional<String> getStudentNameOptional() {
+
+        Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.getAllStudents().getFirst());
+
+        return studentOptional.map(Student::getName);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(getStudentName().length());
+//        String name = getStudentName();
+//        if(name == null){
+//            System.out.println("Student name is null");
+//        }
+//        System.out.println("Name is "+name.length()+" characters");
+
+        Optional<String> name = getStudentNameOptional();
+        if (name.isPresent()) {
+            System.out.println(name.get());
+        }
+
+//        System.out.println(getStudentNameOptional());
     }
 }
