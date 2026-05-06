634eb767721f36972716143fbb52e50725d069b7
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-05-06 11:55:44 +0530

    optional map and flatmap example completed

--- src/optional/OptionalMapAndFlatMapExample.java
+++ src/optional/OptionalMapAndFlatMapExample.java
@@ -1,4 +1,24 @@
 package optional;
 
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.Optional;
+
 public class OptionalMapAndFlatMapExample {
+
+
+    //filter
+    public static boolean optionalfilter(){
+        Optional<Student>  studentOptional = Optional.ofNullable(StudentDataBase.getAllStudents().get(0));
+
+        studentOptional.filter(s->s.getGpa()>=3.5).ifPresent(System.out::println);
+        return false;
+    }
+    //flatmap
+    //map
+
+    public static void main(String[] args) {
+        System.out.println(optionalfilter());
+    }
 }
