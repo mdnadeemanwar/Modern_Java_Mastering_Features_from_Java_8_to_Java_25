1141458a348b9cf26521cb6a702a1afd674b67e8
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-05-09 00:22:57 +0530

    java sealed class example

---
+++ src/java17SealedClassInterface/Dog.java
@@ -0,0 +1,5 @@
+package java17SealedClassInterface;
+
+//Here complier will give error
+//public class Dog extends Vehichle{
+//}
---
+++ src/java17SealedClassInterface/Vehichle.java
@@ -0,0 +1,5 @@
+package java17SealedClassInterface;
+
+public  sealed class Vehichle permits Bus , Truck {
+
+}
