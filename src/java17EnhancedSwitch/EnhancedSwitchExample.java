76bb32894c083f5497de571c476b57771195ef79
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-05-08 19:32:13 +0530

    Enhanced switch example with remove default

--- src/java17EnhancedSwitch/EnhancedSwitchExample.java
+++ src/java17EnhancedSwitch/EnhancedSwitchExample.java
@@ -41,21 +41,38 @@ public class EnhancedSwitchExample {
         return noOfDays;
 
     }
-    public static int withEnhancedSwitchWithYield(Month month, int year) {
-        int noOfDays = 0;
 
-        switch (month) {
-            case SEPTEMBER, APRIL, JUNE, NOVEMBER -> noOfDays = 29;
+    public static int withEnhancedSwitchWithYield(Month month, int year) {
+
+        return switch (month) {
+            case SEPTEMBER, APRIL, JUNE, NOVEMBER -> 29;
             case FEBRUARY -> {
                 //you can write the multiple statement here
                 System.out.println("checking is " + year + " is leap");
-                noOfDays = Year.isLeap(year) ? 29 : 28;
+                yield Year.isLeap(year) ? 29 : 28;
             }
-            default -> noOfDays = 31;
-        }
-        return noOfDays;
+            default -> 31;
+        };
 
     }
+
+    public static int withEnhancedSwitchWithRemoveDefault(Month month, int year) {
+
+        return switch (month) {
+            case SEPTEMBER, APRIL, JUNE, NOVEMBER -> 29;
+            case FEBRUARY -> {
+                //you can write the multiple statement here
+                System.out.println("checking is " + year + " is leap");
+                yield Year.isLeap(year) ? 29 : 28;
+            }
+            /*
+            if we cover all the scenerio like remaining month
+            then we dont need to write the default
+             */
+            case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER -> 31;
+        };
+    }
+
 
     public static void main(String[] args) {
         System.out.println("With Normal Switch Example");
@@ -66,5 +83,8 @@ public class EnhancedSwitchExample {
 
         System.out.println("EnhancedSwitchYield Example");
         System.out.println(withEnhancedSwitchWithYield(Month.FEBRUARY, 1900));
+
+        System.out.println("EnhancedSwitchWithRemoveDefault Example");
+        System.out.println(withEnhancedSwitchWithRemoveDefault(Month.FEBRUARY, 1900));
     }
 }
