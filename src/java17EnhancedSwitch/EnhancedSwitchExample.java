5e08e5bf674eee55980f9b278b7b8a27142535e3
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-05-08 19:11:32 +0530

    normal switch example

---
+++ src/java17EnhancedSwitch/EnhancedSwitchExample.java
@@ -0,0 +1,37 @@
+package java17EnhancedSwitch;
+
+import java.time.Month;
+import java.time.Year;
+
+public class EnhancedSwitchExample {
+
+
+    /*
+    Old way to impliment the switch
+     */
+
+    public static int getInDays(Month month, int year) {
+        int noOfDays = 0;
+        switch (month) {
+            case SEPTEMBER:
+            case APRIL:
+            case JUNE:
+            case NOVEMBER:
+                noOfDays = 30;
+                break;
+            case FEBRUARY:
+                System.out.println("checking is " + year + "is leap");
+                noOfDays = Year.isLeap(year) ? 29 : 28;
+                break;
+            default:
+                noOfDays = 31;
+                break;
+        }
+        return noOfDays;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("EnhancedSwitch Example");
+        System.out.println(getInDays(Month.FEBRUARY, 1900));
+    }
+}
