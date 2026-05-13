2051db410b75531972c3e2cf143bed4825df94ca
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-05-13 13:01:35 +0530

    test case of async function is completed

--- src/main/java/org/learnjava/http2client/HttpExample.java
+++ src/main/java/org/learnjava/http2client/HttpExample.java
@@ -1,5 +1,6 @@
 package org.learnjava.http2client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 
 public class HttpExample {
 
@@ -37,12 +39,36 @@ public class HttpExample {
         }
     }
 
+    public static CompletableFuture<UserDetails> getUserDetailsByIdAsync() throws IOException, InterruptedException {
+
+        var request = requestBuilder(HttpExample.jsonsampleApiByid);
+        try {
+            var response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.thenApply(httpResponse -> {
+                System.out.println("Response of api is " + httpResponse.body());
+                try {
+                    return mapper.readValue(
+                            httpResponse.body(),
+                            UserDetails.class
+                    );
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public static HttpRequest requestBuilder(String url) {
         return HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        getUserDetailsById();
+//        getUserDetailsById();
+        getUserDetailsByIdAsync();
     }
 }
